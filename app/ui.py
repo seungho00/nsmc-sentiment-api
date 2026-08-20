52e943d24d191d40ae91d88f0214a435cdb5c783
seungho00 <133303280+seungho00@users.noreply.github.com>
2026-08-20 23:55:13 +0900

    ui 구현

---
+++ app/ui.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import requests
+
+
+st.title("영화 리뷰 감성분석 AI")
+
+sentence = st.text_area("영화 리뷰를 입력하세요.")
+
+if st.button("분석"):
+    response = requests.post(
+        "http://127.0.0.1:8000/predict",
+        json={"sentence": sentence}
+    )
+
+    result = response.json()
+
+    st.write("### 분석 결과")
+    st.write(f"긍정: {result['positive']:.2%}")
+    st.write(f"부정: {result['negative']:.2%}")
